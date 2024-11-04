24e74a274c96be1e82edb109377b5944329e0882
ohnoruu <rubydao007@gmail.com>
2024-11-04 11:22:22 -0800

    abc

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    //Problem 1
+    int numInput=sc.nextInt();
+    int sum = 0;
+    while(numInput != -1){
+      numInput = sc.nextInt();
+      sum += numInput;
+    }
+    System.out.println("Sum is " + sum);
   }
 }
